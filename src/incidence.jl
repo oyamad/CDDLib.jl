f0470055432923c707c94f92a11fc32e0276599b
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2026-01-25 16:47:02 +0900

    tmp

--- src/incidence.jl
+++ src/incidence.jl
@@ -1,14 +1,30 @@
+_getrepfor(p::Polyhedron{T}, ::Polyhedra.Incident{T, <:HRepElement{T}}) where {T} = getine(p)
+_getincidence(p::Polyhedron{T}, ::Polyhedra.Incident{T, <:HRepElement{T}}) where {T} = gethincidence(p)
 _incel(p::Polyhedron{T}, inc::Polyhedra.IncidentElements{T}, idx) where {T} = get(p, idx)
 _incel(p::Polyhedron{T}, inc::Polyhedra.IncidentIndices{T}, idx) where {T} = idx
 
 function Base.get(p::CDDLib.Polyhedron{T}, inc::Polyhedra.Incident{T, ElemT}; tol) where {T, ElemT<:HRepElement{T}}
-    ine = CDDLib.getine(p)
-    incidence = CDDLib.getincidence(p)
+    rep = _getrepfor(p, inc)
+    incidence = _getincidence(p, inc)
     incT = Polyhedra._inctype(inc)
     incs = incT[]
     for i in incidence[inc.idx.value]
         idx = Polyhedra.Index{T, ElemT}(i)
-        isvalid(ine, idx) && push!(incs, _incel(p, inc, idx))
+        isvalid(rep, idx) && push!(incs, _incel(p, inc, idx))
+    end
+    return incs
+end
+
+_getrepfor(p::Polyhedron{T}, ::Polyhedra.Incident{T, <:VRepElement{T}}) where {T} = getext(p)
+_getincidence(p::Polyhedron{T}, ::Polyhedra.Incident{T, <:VRepElement{T}}) where {T} = getvincidence(p)
+function Base.get(p::CDDLib.Polyhedron{T}, inc::Polyhedra.Incident{T, ElemT}; tol) where {T, ElemT<:VRepElement{T}}
+    rep = _getrepfor(p, inc)
+    incidence = _getincidence(p, inc)
+    incT = Polyhedra._inctype(inc)
+    incs = incT[]
+    for i in incidence[inc.idx.value]
+        idx = Polyhedra.Index{T, ElemT}(i)
+        isvalid(rep, idx) && push!(incs, _incel(p, inc, idx))
     end
     return incs
 end
