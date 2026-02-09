05f3d1180a23b5ffed2ada61feec3e9561d8e86c
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2026-02-09 15:59:58 +0900

    tmp

--- test/incidence.jl
+++ test/incidence.jl
@@ -2,6 +2,37 @@ using Test
 using Polyhedra
 using CDDLib
 
+function test_incident_functions(p::CDDLib.Polyhedron)
+    T = Polyhedra.coefficient_type(p)
+    tol = Polyhedra._default_tol(T)
+
+    for (hsidx, hs) in zip(eachindex(halfspaces(p)), halfspaces(p))
+        inc_pts = @inferred incidentpoints(p, hsidx)
+        inc_ptinds = @inferred incidentpointindices(p, hsidx)
+        for pts in (inc_pts, get.(p, inc_ptinds))
+            for pt in pts
+                @test Polyhedra.isincident(pt, hs, tol=tol)
+            end
+        end
+        # Test against the generic versions in Polyhedra.jl
+        @test inc_pts == incidentpoints(p, hsidx, tol=tol)
+        @test inc_ptinds == incidentpointindices(p, hsidx, tol=tol)
+    end
+
+    for (ptidx, pt) in zip(eachindex(points(p)), points(p))
+        inc_hss = @inferred incidenthalfspaces(p, ptidx)
+        inc_hsinds = @inferred incidenthalfspaceindices(p, ptidx)
+        for hss in (inc_hss, get.(p, inc_hsinds))
+            for hs in hss
+                @test Polyhedra.isincident(pt, hs, tol=tol)
+            end
+        end
+        # Test against the generic versions in Polyhedra.jl
+        @test inc_hss == incidenthalfspaces(p, ptidx, tol=tol)
+        @test inc_hsinds == incidenthalfspaceindices(p, ptidx, tol=tol)
+    end
+end
+
 @testset "Incidence tests" begin
     @testset "Incidence $precision" for precision in [:float, :exact]
         A = [1 1; 1 -1; -1 0]; b = [1, 0, 0]
@@ -67,6 +98,41 @@ using CDDLib
                 for i in p.vincidence[hidx]
                     @test Polyhedra.isincident(vs[i], h, tol=tol)
                 end
+            end
+        end
+    end
+
+    @testset "incident* functions $precision" for precision in [:float, :exact]
+        @testset "Points + lines" begin
+            A = [-1 0; 1 0]; b = [0, 1]
+            p = polyhedron(hrep(A, b), CDDLib.Library(precision))
+            T = Polyhedra.coefficient_type(p)
+            tol = Polyhedra._default_tol(T)
+
+            for (hsidx, hs) in zip(eachindex(halfspaces(p)), halfspaces(p))
+                inc_pts = @inferred incidentpoints(p, hsidx)
+                inc_ptinds = @inferred incidentpointindices(p, hsidx)
+                for pts in (inc_pts, get.(p, inc_ptinds))
+                    for pt in pts
+                        @test Polyhedra.isincident(pt, hs, tol=tol)
+                    end
+                end
+                # Test against the generic versions in Polyhedra.jl
+                @test inc_pts == incidentpoints(p, hsidx, tol=tol)
+                @test inc_ptinds == incidentpointindices(p, hsidx, tol=tol)
+            end
+
+            for (ptidx, pt) in zip(eachindex(points(p)), points(p))
+                inc_hss = @inferred incidenthalfspaces(p, ptidx)
+                inc_hsinds = @inferred incidenthalfspaceindices(p, ptidx)
+                for hss in (inc_hss, get.(p, inc_hsinds))
+                    for hs in hss
+                        @test Polyhedra.isincident(pt, hs, tol=tol)
+                    end
+                end
+                # Test against the generic versions in Polyhedra.jl
+                @test inc_hss == incidenthalfspaces(p, ptidx, tol=tol)
+                @test inc_hsinds == incidenthalfspaceindices(p, ptidx, tol=tol)
             end
         end
     end
